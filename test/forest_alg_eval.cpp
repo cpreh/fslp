32600b08b419744f77ad3d6dd359e838c3148883
Carl Philipp Reh <carlphilippreh@gmail.com>
2020-10-08 11:13:45 +0200

    Updates

--- test/forest_alg_eval.cpp
+++ test/forest_alg_eval.cpp
@@ -19,8 +19,8 @@
 namespace
 {
 template <typename Ch>
-fslp::forest_fix<Ch>
-eval(fslp::forest_alg_t<Ch, fslp::forest_fix<Ch>, fslp::forest_x_fix<Ch>> const &v)
+auto eval(fslp::forest_alg_t<Ch, fslp::forest_fix<Ch>, fslp::forest_x_fix<Ch>> const &v)
+    -> fslp::forest_fix<Ch>
 {
   using forest = fslp::forest_fix<Ch>;
   using forest_x = fslp::forest_x_fix<Ch>;
@@ -40,9 +40,9 @@ eval(fslp::forest_alg_t<Ch, fslp::forest_fix<Ch>, fslp::forest_x_fix<Ch>> const 
       });
 }
 
-template<typename Ch>
-fslp::forest_x_fix<Ch>
-eval(fslp::forest_alg_x_t<Ch, fslp::forest_x_fix<Ch>, fslp::forest_fix<Ch>> const &v)
+template <typename Ch>
+auto eval(fslp::forest_alg_x_t<Ch, fslp::forest_x_fix<Ch>, fslp::forest_fix<Ch>> const &v)
+    -> fslp::forest_x_fix<Ch>
 {
   using forest = fslp::forest_fix<Ch>;
   using forest_x = fslp::forest_x_fix<Ch>;
@@ -51,19 +51,16 @@ eval(fslp::forest_alg_x_t<Ch, fslp::forest_x_fix<Ch>, fslp::forest_fix<Ch>> cons
   using forest_x_r = fslp::forest_x_r<tree_x>;
 
   return fcppt::variant::match(
-    [](std::tuple<Ch,forest,forest> const &r)
-    {
-      forest_x const i{std::make_tuple(std::get<1>(r),forest_x_r{fslp::var{}},std::get<2>(r))};
-      forest const e{std::vector<tree>{}};
-      return fslp::forest_x{std::make_tuple(e,fslp::forest_x_r<tree_x>{tree_x{std::make_tuple(std::get<0>(r),i)}}, e)};
-    },
-    [](std::tuple<forest_x,forest_x> const &r)
-    {
-      return fslp::compose<Ch>(std::get<0>(r),std::get<1>(r));
-    }
-  );
+      [](std::tuple<Ch, forest, forest> const &r) {
+        forest_x const i{std::make_tuple(std::get<1>(r), forest_x_r{fslp::var{}}, std::get<2>(r))};
+        forest const e{std::vector<tree>{}};
+        return forest_x{
+            std::make_tuple(e, forest_x_r{tree_x{std::make_tuple(std::get<0>(r), i)}}, e)};
+      },
+      [](std::tuple<forest_x, forest_x> const &r) {
+        return fslp::compose<Ch>(std::get<0>(r), std::get<1>(r));
+      });
 }
-
 }
 
 TEST_CASE("fslp::forest_alg_eval","[fslp]")
@@ -74,6 +71,8 @@ TEST_CASE("fslp::forest_alg_eval","[fslp]")
   using forest_alg_t = fslp::forest_alg_t<char,forest,forest_x>;
   using forest_alg_x_t = fslp::forest_alg_x_t<char,forest_x,forest>;
 
+  forest const e{std::vector<tree>{}};
+
   CHECK(eval<char>(forest_alg_t{fcppt::unit{}}) == forest{std::vector<tree>{}});
-//  CHECK(eval<char>(forest_alg_x_t{fcppt::var{}}) ==
+//  CHECK(eval<char>(forest_alg_x_t{std::make_tuple('a',e,e)}) ==
 }

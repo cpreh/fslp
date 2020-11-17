f366146d16a251e3b93c963e3ba8780726fa0520
Carl Philipp Reh <carlphilippreh@gmail.com>
2020-11-17 15:02:43 +0100

    clang-format

--- test/slp.cpp
+++ test/slp.cpp
@@ -17,27 +17,32 @@
 
 namespace
 {
+enum class N1
+{
+  A
+};
+enum class N2
+{
+  A
+};
 
-enum class N1 { A };
-enum class N2 { A };
+template <typename F, typename T>
+using forest_c = fslp::forest_t<char, F, T>;
 
-template<typename F,typename T>
-using forest_c = fslp::forest_t<char,F,T>;
-
-template<typename T,typename F>
-using tree_c = fslp::tree_t<char,T,F>;
+template <typename T, typename F>
+using tree_c = fslp::tree_t<char, T, F>;
 
 using test_slp = fslp::slp<forest_c, tree_c>::type<N1, N2>;
 
-using forest_N = forest_c<N1,N2>;
-using tree_N = tree_c<N2,N1>;
+using forest_N = forest_c<N1, N2>;
+using tree_N = tree_c<N2, N1>;
 
 static_assert(std::is_same_v<
               test_slp,
               std::tuple<fcppt::function<forest_N(N1)>, fcppt::function<tree_N(N2)>>>);
 }
 
-TEST_CASE("fslp::fold slp","[fslp]")
+TEST_CASE("fslp::fold slp", "[fslp]")
 {
   test_slp const slp{std::make_tuple(
       fcppt::function<forest_N(N1)>{[](N1) { return forest_N{std::vector<N2>{}}; }},
@@ -51,6 +56,6 @@ TEST_CASE("fslp::fold slp","[fslp]")
   using forest = fslp::forest_fix<char>;
   using tree = fslp::tree_fix<char>;
 
-  using types = metal::map<metal::pair<N1, forest>,metal::pair<N2, tree>>;
-  CHECK(fslp::fold<types>(N1::A,eval) == forest{std::vector<tree>{}});
+  using types = metal::map<metal::pair<N1, forest>, metal::pair<N2, tree>>;
+  CHECK(fslp::fold<types>(N1::A, eval) == forest{std::vector<tree>{}});
 }
